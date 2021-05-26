4b273ccc66b4dbcaa712e89c321d22538f1f92ab
zhaoxumeng <xumeng.zhao@luckincoffee.com>
2021-05-26 17:03:27 +0800

    初始化

---
+++ src/main/java/com/rm/idataecs/idataecs/base/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.rm.idataecs.idataecs.base.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @program: idataecs
+ * @description:
+ * @author: xumeng.zhao
+ * @create: 2021-05-26 15:40
+ */
+@Controller
+public class TestController {
+
+
+
+}

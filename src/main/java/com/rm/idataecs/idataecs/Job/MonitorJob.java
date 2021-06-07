bf33ae0357e49be6b20a775f416e7c4555c159c7
zhaoxumeng <xumeng.zhao@luckincoffee.com>
2021-06-07 20:35:25 +0800

    增

--- src/main/java/com/rm/idataecs/idataecs/Job/MonitorJob.java
+++ src/main/java/com/rm/idataecs/idataecs/Job/MonitorJob.java
@@ -1,15 +1,8 @@
 package com.rm.idataecs.idataecs.Job;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.rm.idataecs.idataecs.monitor.entity.MonitorConfigEntity;
-import com.rm.idataecs.idataecs.monitor.entity.SqlCheckEntity;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.impl.StdSchedulerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 /**
  * @program: idataecs

024e68413292f052ec3daca9e7c9baa76c239fa2
zhaoxumeng <xumeng.zhao@luckincoffee.com>
2021-06-09 11:08:51 +0800

    调度

--- src/main/java/com/rm/idataecs/idataecs/IdataecsApplication.java
+++ src/main/java/com/rm/idataecs/idataecs/IdataecsApplication.java
@@ -3,6 +3,7 @@ package com.rm.idataecs.idataecs;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class IdataecsApplication {

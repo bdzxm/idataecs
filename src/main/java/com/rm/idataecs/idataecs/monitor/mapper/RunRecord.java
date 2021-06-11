5f68bc752126759a7b78fe327d7f40bc1c3e0b91
zhaoxumeng <xumeng.zhao@luckincoffee.com>
2021-06-11 10:38:15 +0800

    新增查询

--- src/main/java/com/rm/idataecs/idataecs/monitor/mapper/RunRecord.java
+++ src/main/java/com/rm/idataecs/idataecs/monitor/mapper/RunRecord.java
@@ -1,8 +1,10 @@
 package com.rm.idataecs.idataecs.monitor.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rm.idataecs.idataecs.monitor.entity.MonitorConfigEntity;
 import com.rm.idataecs.idataecs.monitor.entity.RunRecordEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -14,5 +16,18 @@ public interface RunRecord extends BaseMapper<RunRecordEntity> {
      * @return
      */
     List<RunRecordEntity> getRecordByQueryTime();
+
+    /**
+     * 返回需要报警的数据
+     * @return
+     */
+    List<RunRecordEntity> getProbRecord();
+
+    /**
+     * 根据表名查询
+     * @param tableName
+     * @return
+     */
+    MonitorConfigEntity getTableByName(@Param("table_name")String tableName);
 
 }
